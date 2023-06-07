c60c815d5d10bb0df88d1203d674d1691b301bc1
Gathsara2002 <liviru2002mmg@gmail.com>
2023-06-07 13:31:38 +0530

    add item detail to table

--- assignment/js/inMemory-POS/assets/myJavaScript.js
+++ assignment/js/inMemory-POS/assets/myJavaScript.js
@@ -33,5 +33,38 @@ document.getElementById("orders").addEventListener("click", function () {
     document.getElementById("orderPage").style.display = 'block';
 });
 
+/*add customer to table*/
+/*bind event to save customer*/
+$("#btnSaveCustomer").click(function () {
+
+    /*get customer details from input fields*/
+    let name = $("#customerName").val();
+    let address = $("#addressCus").val();
+    let nic = $("#nicCus").val();
+    let tel = $("#tpNo").val();
+
+    /*create new row*/
+    let tRow = "<tr>" + "<td>" + name + "</td>" + "<td>" + address + "</td>" + "<td>" + nic + "</td>" + "<td>" + tel + "</td>" + "</tr>";
+
+    /*append to tbody*/
+    $("#tblCustomer").append(tRow);
+});
+
+
+/*add item to table*/
+$("#btnSaveItem").click(function () {
+
+    /*get item details from input fields*/
+    let code = $("#itemCode1").val();
+    let name = $("#itemName1").val();
+    let price = $("#price").val();
+    let qty = $("#qty").val();
+
+    /*create new row*/
+    let tRow = "<tr>" + "<td>" + code + "</td>" + "<td>" + name + "</td>" + "<td>" + price + "</td>" + "<td>" + qty + "</td>" + "</tr>";
+
+    /*append to tbody*/
+    $("#tblItem").append(tRow);
+});
 
 
