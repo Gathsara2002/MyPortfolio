e1024cb9282b2ab382b0fdfd79e2809768d36872
Gathsara2002 <liviru2002mmg@gmail.com>
2023-06-12 11:32:49 +0530

    added events to all buttons

---
+++ assignment/js/cal/assets/js/myScript.js
@@ -0,0 +1,70 @@
+/*clear input*/
+$("#btn1").click(function () {
+    $("#display").val("");
+});
+
+/*set event to buttons*/
+
+$("#btn2").click(function () {
+    $("#display").val("7");
+});
+
+$("#btn3").click(function () {
+    $("#display").val("8");
+});
+
+$("#btn4").click(function () {
+    $("#display").val("9");
+});
+
+$("#btn5").click(function () {
+    $("#display").val("+");
+});
+
+$("#btn6").click(function () {
+    $("#display").val("6");
+});
+
+$("#btn7").click(function () {
+    $("#display").val("5");
+});
+
+$("#btn8").click(function () {
+    $("#display").val("4");
+});
+
+$("#btn9").click(function () {
+    $("#display").val("-");
+});
+
+$("#btn10").click(function () {
+    $("#display").val("3");
+});
+
+$("#btn11").click(function () {
+    $("#display").val("2");
+});
+
+$("#btn12").click(function () {
+    $("#display").val("1");
+});
+
+$("#btn13").click(function () {
+    $("#display").val("/");
+});
+
+$("#btn14").click(function () {
+    $("#display").val("0");
+});
+
+$("#btn15").click(function () {
+    $("#display").val(".");
+});
+
+$("#btn16").click(function () {
+    $("#display").val("=");
+});
+
+$("#btn17").click(function () {
+    $("#display").val("*");
+});
